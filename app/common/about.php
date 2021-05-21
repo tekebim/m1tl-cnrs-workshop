5ea4243824f9b7eff24fcff00750070420793b4a
Michael R <tekmedias@gmail.com>
2021-05-21 23:41:09 +0200

    :art: style(admin): adjust input colors on form configuration

--- app/common/about.php
+++ app/common/about.php
@@ -8,5 +8,5 @@ require_once($_ENV["RELATIVE_PATH"] . "common/init.php");
 $pid = (isset($_GET["pid"]) ? intval($_GET["pid"]) : 1);
 
 echo '<main id="page-about">';
-echo $twig->render('about.twig', array("pid" => $pid, "projectName" => _SITE_NAME_));
+echo $twig->render('about.twig', array("pid" => $pid, "projectName" => _PROJECT_NAME_));
 echo '</main>';
